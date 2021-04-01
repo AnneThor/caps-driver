c1960113eea3a0818af495f1c71685749bf59f49
AnneThor <anne.thor@gmail.com>
2021-03-31 22:39:01 -0700

    finished basic functionality

--- driver.js
+++ driver.js
@@ -4,24 +4,26 @@ require('dotenv').config();
 const io = require('socket.io-client');
 
 const port = process.env.PORT || 3000;
-const host = `http://localhost:${port}/caps         `;
+const host = `http://localhost:${port}/caps`;
 const socket = io.connect(host);
+const driver = 'driver';
+socket.emit('join', driver);
 
 socket.on('pickup', pickup);
-socket.on('in-transit', intransit);
+socket.on('in-transit', inTransit);
 
 function pickup(payload, event) {
   setTimeout(() => {
-    console.log(`DRIVER: picked up ${payload.orderId}`);
+    console.log(`DRIVER: picked up ${payload.orderID}`);
     let event = { event: 'in-transit' };
     socket.emit('in-transit', payload, event);
-  }, 1000);
+  }, 1500);
 }
 
-function inTransit(payload) {
+function inTransit(payload, event) {
   setTimeout(() => {
-    console.log(`DRIVER: delivered order ${payload.orderId}`);
-    let event = { event: 'delivered' };
+    console.log(`DRIVER: delivered order ${payload.orderID}`);
+    let event = { event: 'delivered' }
     socket.emit('delivered', payload, event);
   }, 3000);
 }
